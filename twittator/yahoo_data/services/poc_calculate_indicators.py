96457ec09efa4b405fc35084b3af4eca6b93ec57
Eduardo Batistella <ehbatistella@gmail.com>
2023-03-28 21:36:13 -0300

    created trend indicators. Need to implement the other indicators

--- twittator/yahoo_data/services/poc_calculate_indicators.py
+++ twittator/yahoo_data/services/poc_calculate_indicators.py
@@ -1,13 +1,33 @@
 from poc_get_data import get_price_data
+import pandas as pd
+from ta import trend
 
-tickers = ['MGLU3.SA', 'PETR4.SA', 'ITUB4.SA']
+ticker = 'MGLU3.SA'
 start_date = '2022-01-01'
 end_date = '2023-01-05'
 
 results = get_price_data(
-    tickers = tickers,
+    ticker = ticker,
     start_date = start_date,
     end_date = end_date
     )
 
-print(results)
+# def get_technical_analysis(data: pd.DataFrame):
+
+test_mglu = results
+
+moving_average = trend.sma_indicator(
+    test_mglu['Close'], 
+    window = 1,
+    )
+weighted_average = trend.wma_indicator(
+    test_mglu['Close'],
+    window = 1
+)
+cci = trend.CCIIndicator(
+    high = test_mglu['High'],
+    low = test_mglu['Low'],
+    close = test_mglu['Close'],
+    window = 14,
+    constant = 0.015
+)
--- twittator/yahoo_data/services/poc_get_data.py
+++ twittator/yahoo_data/services/poc_get_data.py
@@ -2,30 +2,32 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime
 def get_price_data(
-        tickers: list, 
+        ticker: str, 
         start_date: str, 
         end_date: str = datetime.now()
         ):
     
     df = pd.DataFrame()
 
-    for ticker in tickers:
-        ticker_historical_prices = yf.Ticker(
-            ticker
-            ).history(
-            start = start_date, 
-            end = end_date,
-            )['Close']
-        
-        df[ticker] = ticker_historical_prices
+    ticker_historical_prices = yf.Ticker(
+        ticker
+        ).history(
+        start = start_date, 
+        end = end_date,
+        )
+    
+    df['High'] = ticker_historical_prices['High']
+    df['Low'] = ticker_historical_prices['Low']
+    df['Open'] = ticker_historical_prices['Open']
+    df['Close'] = ticker_historical_prices['Close']
 
     return df
 
-tickers = ['MGLU3.SA', 'PETR4.SA', 'ITUB4.SA']
+ticker = 'MGLU3.SA'
 start_date = '2022-01-01'
 end_date = '2023-01-05'
 results = get_price_data(
-    tickers = tickers,
+    ticker = ticker,
     start_date = start_date,
     end_date = end_date
     )
