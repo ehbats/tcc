2bc66250a7db0bb565232b62fd590f9e68b19b9e
Eduardo Batistella <ehbatistella@gmail.com>
2023-04-08 13:54:15 -0300

    Finished all of the data getters. Might better implement the keywords on the news data

--- twittator/news_data/services/poc_get_news.py
+++ twittator/news_data/services/poc_get_news.py
@@ -1,4 +1,5 @@
 import feedparser
+
 class GetNewsData():
     BASE_URL =  "https://news.google.com/rss/"
     def get_news_data(
@@ -15,15 +16,21 @@ class GetNewsData():
         base_url = self.BASE_URL
         
         if is_query:
-            base_url = f'{base_url}search?q={query}+after:{after}+before:{before}'
+            base_url = f'{base_url}search?q={query}+after:{after}+before:{before}&'
+
         if topic != "":
-            base_url = f'{base_url}/headlines/topic
+            base_url = f'{base_url}headlines/section/topic/BUSINESS?'
+        
+        final_url = f'{base_url}hl={language}&gl={country}&ceid={ceid}'
+        print(final_url)
+        feed = feedparser.parse(final_url)
 
+        return feed
 
-rss_url = "https://news.google.com/rss/search?q=EXEMPLO&hl=pt-BR&gl=BR&ceid=PT:br"
-feed = feedparser.parse(rss_url)
+# rss_url = "https://news.google.com/rss/search?q=EXEMPLO&hl=pt-BR&gl=BR&ceid=PT:br"
+# feed = feedparser.parse(rss_url)
 
-for entry in feed.entries:
-    print(entry.title)
-    print(entry.summary)
-    print()
+# for entry in feed.entries:
+#     print(entry.title)
+#     print(entry.summary)
+#     print()
