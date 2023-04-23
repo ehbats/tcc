961c7fca081adc2a2bf286864a6571d35313d1d9
Eduardo Batistella <ehbatistella@gmail.com>
2023-04-23 18:55:43 -0300

    Implemented text preprocessing, including lemmatization, tokenization and stop words removal. Need to find the best way to calculate polarization for sentences

--- twittator/lang_processing/services/get_polarization.py
+++ twittator/lang_processing/services/get_polarization.py
@@ -1,16 +1,48 @@
 import nltk
+import spacy
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import RSLPStemmer
+from senticnet.senticnet import SenticNet
 
 class GetPolarization:
-    def __init__(self, lang: str = 'portuguese'):
-        nltk.download('snowball_data')
+    def __init__(
+            self, 
+    ):
         nltk.download('stopwords')
         nltk.download('rslp')
-        self.stopwords = set(stopwords.words(lang))
+        nltk.download('punkt')
+
+    def text_preprocessing(
+            self, 
+            text: str,
+            lemmatization_lang: str = 'pt',
+            lemmatization_size: str = 'lg',
+            lang: str = 'portuguese',
+    ):
+        text = text.lower()
+        stopwords = set(stopwords.words(lang))
+
+        add_stopwords = ['em o', '.', '%', ',', 'em este', 'em aquele']
+        for stopword in add_stopwords:
+            stopwords.add(stopword)
+
+        nlp = spacy.load(f'{lemmatization_lang}_core_news_{lemmatization_size}')
         
-        self.rslp = RSLPStemmer()
-        
+        lemmatized_and_tokenized_text = nlp(text)
+
+        lemmatized_tokenized_no_stopwords = [token.lemma_ for token in lemmatized_and_tokenized_text if token.lemma_ not in stopwords]
+
+        return lemmatized_tokenized_no_stopwords
+
+    def get_polatization(
+            self,
+
+    ):
+        x=1
+    def run_with_default_params(
+            self,
+            text
+    ):
+        x=1
 
 instance = GetPolarization()
+# print(instance.lemmas)
