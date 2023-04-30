2e45dfff448a9dd2e5da25f780b184d7b6483441
Eduardo Batistella <ehbatistella@gmail.com>
2023-04-29 22:56:11 -0300

    managed to filter only the p tags from the article tag. Need to figure out how to filter only the news content, because there is suggested content and irrelevant buttons such as share on the p tags that are irrelevant for analysis

--- twittator/news_data/services/get_news_content.py
+++ twittator/news_data/services/get_news_content.py
@@ -1,4 +1,5 @@
 import urllib.request as req
+from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
 from bs4.element import Comment
@@ -11,10 +12,26 @@ class GetNewsContent:
             url
             ):
         
-        request = req.urlopen(url).read()
+        fake_request = Request(
+            url = url,
+            headers = {'User-Agent': 'Mozilla/5.0'}
+        )
+        
+        request = urlopen(fake_request).read()
         
         original_url = self.get_original_content_url(request)
-        
+
+        fake_request = Request(
+            url = original_url,
+            headers = {'User-Agent': 'Mozilla/5.0'}
+        )
+
+        request = urlopen(fake_request).read()
+        soup = BeautifulSoup(request, 'html.parser')
+        texts = soup.find('article')
+        for p in texts.select("p"):
+            print(p.text)
+
     def get_original_content_url(self, raw_html):
         soup = BeautifulSoup(raw_html, 'html.parser')
         texts = soup.findAll(string = True)
@@ -25,7 +42,7 @@ class GetNewsContent:
         return news_url
     
     def get_original_content_text(self, raw_html):
-        
+        pass
 
     def tag_visible(self, element):
         if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -33,6 +50,18 @@ class GetNewsContent:
         if isinstance(element, Comment):
             return False
         return True
+    
+    def tag_p(self, element):
+        if element.parent.name in ['p', ['body']]:
+            return True
+        else:
+            return False
+        
+    def tag_article(self, element):
+        if element.parent.name in ['article']:
+            return True
+        else:
+            return False
 
 test_url = 'https://news.google.com/rss/articles/CBMioAFodHRwczovL3d3dy5zZXVkaW5oZWlyby5jb20vMjAyMy9maW5hbmNhcy1wZXNzb2Fpcy9uZW0tbnViYW5rLW5lbS1pbnRlci1lc3Nlcy1zYW8tb3MtMTAtbWVsaG9yZXMtY2FydG9lcy1kZS1jcmVkaXRvLXNlbS1hbnVpZGFkZS1lLXBhcmEtYWN1bXVsYXItbWlsaGFzLWRlLTIwMjMv0gEA?oc=5'
 
