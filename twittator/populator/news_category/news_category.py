6f5eb48d268f84870707d298e8ebb2b5942c3e91
Eduardo Batistella <ehbatistella@gmail.com>
2023-08-17 22:43:21 -0300

    managed to read the csv, it was accusing a weird path error. Also added .json to gitignore to avoid commiting such huge files

--- twittator/populator/news_category/news_category.py
+++ twittator/populator/news_category/news_category.py
@@ -2,6 +2,11 @@ import pandas as pd
 import sys, os
 sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath(".."))
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twittator.settings")
+django.setup()
 from news_data.models.kaggle_news import KaggleNews
 
-df = pd.read_csv("News_Category_Dataset_v3")
+df = pd.read_json("./populator/news_category/News_Category_Dataset_v3.json", lines=True)
+
+print(df)
