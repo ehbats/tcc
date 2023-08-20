b0ece8da7fc6803bbba58dc3a49c9defdb2f37ed
Eduardo Batistella <ehbatistella@gmail.com>
2023-08-20 18:44:35 -0300

    Implemented necessary changes to polarity app to allow populating with kaggle news instead of regular news

--- twittator/lang_processing/models/polarity.py
+++ twittator/lang_processing/models/polarity.py
@@ -3,13 +3,14 @@ sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath(".."))
 from django.db import models
 from news_data.models.news import News
-
+from news_data.models.kaggle_news import KaggleNews
 class Polarity(models.Model):
     id = models.AutoField(primary_key = True)
     relevant_words = models.JSONField(default = list)
     mean_polarization = models.FloatField(default = 0,)
     polarization_list = models.JSONField(default = list)
-    news_id = models.ForeignKey(News, on_delete = models.CASCADE)
+    news_id = models.ForeignKey(News, null=True, on_delete = models.CASCADE)
+    kagglenews_id = models.ForeignKey(KaggleNews, null=True, on_delete = models.CASCADE)
     news_pubdate = models.DateField(null=True, blank=True)
     news_query = models.TextField(null=True, blank=True)
 
