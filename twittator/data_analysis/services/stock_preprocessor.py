d05466a4b917662ed02c8144685bcb93b8567124
Eduardo Batistella <ehbatistella@gmail.com>
2023-10-26 20:46:54 -0300

    added optional technical indicators on the stock preprocessor

--- twittator/data_analysis/services/stock_preprocessor.py
+++ twittator/data_analysis/services/stock_preprocessor.py
@@ -3,6 +3,7 @@ sys.path.append(os.path.abspath("."))
 sys.path.append(os.path.abspath(".."))
 from yahoo_data.services.get_ta_indicators import GenerateTechnicalIndicators
 from yahoo_data.services.get_pca import GeneratePCAByGrouping
+from yahoo_data.services.get_price_data import GetPriceData
 from portfolio_management.services.get_stock_pnl import StockPnLCalculator
 from data_analysis.useful_defaults import GROUPS
 from datetime import datetime
@@ -15,21 +16,29 @@ class StockPreprocessor:
             price_column: str,
             return_periods: int,
             end_date: str = datetime.now(),
+            generate_technical: bool = True
         ):
-            technical_indicators_df = GenerateTechnicalIndicators(
-                ticker,
-                start_date,
-                end_date
-            ).run_with_default_params()
-            technical_indicators_df = technical_indicators_df.dropna()
+            if generate_technical:
+                technical_indicators_df = GenerateTechnicalIndicators(
+                    ticker,
+                    start_date,
+                    end_date
+                ).run_with_default_params()
+                technical_indicators_df = technical_indicators_df.dropna()
 
-            groups = GROUPS
+                groups = GROUPS
 
-            df_after_pca = GeneratePCAByGrouping().generate_pca(
-                df = technical_indicators_df, 
-                groupings = groups,
-                columns_to_leave = ['Open', 'High', 'Low', 'Close'],
-            )
+                df_after_pca = GeneratePCAByGrouping().generate_pca(
+                    df = technical_indicators_df, 
+                    groupings = groups,
+                    columns_to_leave = ['Open', 'High', 'Low', 'Close'],
+                )
+            else:
+                 df_after_pca = GetPriceData(
+                      ticker,
+                      start_date,
+                      end_date
+                 )
             df_n_days = StockPnLCalculator().calculate_pnl(
                 df_after_pca,
                 price_column,
